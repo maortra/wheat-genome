006ce63e4919713008e789663fcf05cde2283f85
eyal <mail@eyal-matalon.com>
2021-04-26 18:03:42 +0300

    Running on all accessions

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import wget
-import re
 import os
+import random
+import re
+from urllib.error import HTTPError
+
 import pandas
+import requests
 import tqdm
+import wget
 
 DESIRED_CHINESE_SPRING_GENES = pandas.read_csv("239_my_only_genes_all_yes_no_dif_TE_df.txt", sep="\r", header=None,
                                                squeeze=True)
+# TODO: Fix line below with Inbar.
 # DESIRED_CHINESE_SPRING_GENES = DESIRED_CHINESE_SPRING_GENES.str.replace("02G", "01G")
 
 TRANSLATION_FILE = pandas.read_csv("geneid_2_chinese.sourceid.txt", sep="\t",
@@ -13,30 +18,11 @@ TRANSLATION_FILE = pandas.read_csv("geneid_2_chinese.sourceid.txt", sep="\t",
 TRANSLATION_FILE["chineseSpringGeneID"] = TRANSLATION_FILE["chineseSpringGeneID"].str.rstrip(".1")
 
 
-def get_full_file_names_in_directory(directory_path, file_name_regex_pattern):
-    downloaded_file_path = wget.download(directory_path)
+def get_regex_from_web_pages(web_page_path, regex):
+    downloaded_file_path = wget.download(web_page_path)
     file_content = open(downloaded_file_path).read()
-
-    matches = re.findall(file_name_regex_pattern, file_content, re.MULTILINE)
-    striped_matches = [directory_path + match.replace("\"", "") for match in matches]
     os.remove(downloaded_file_path)
-    return striped_matches
-
-
-def download_genes():
-    regex = r"\"projectedGenes.*\.gff\""
-    link = r'https://webblast.ipk-gatersleben.de/downloads/wheat/gene_projection/'
-    files_paths = get_full_file_names_in_directory(link, regex)
-    for file_path in files_paths:
-        wget.download(file_path)
-
-
-def download_TEs():
-    regex = r"\"PGSB.*\.gff\.gz\""
-    link = r'https://webblast.ipk-gatersleben.de/downloads/wheat/TE_annotation/Wheat_11__Transposons__via_Triticeae-TE-library/'
-    files_paths = get_full_file_names_in_directory(link, regex)
-    for file_path in files_paths:
-        wget.download(file_path)
+    return re.findall(regex, file_content, re.MULTILINE)
 
 
 def open_gene(gene_file_name):
@@ -52,21 +38,28 @@ def open_gene(gene_file_name):
 
     only_genes = only_genes.join(TRANSLATION_FILE.set_index("10wheatGeneID"), "10wheatGeneID")
 
-    # TODO: Fix line below with inbar.
+    # TODO: Fix line below with Inbar.
     # only_desired_genes = only_genes[only_genes["chineseSpringGeneID"].isin(DESIRED_CHINESE_SPRING_GENES)]
 
+    # TODO change "only_genes" to "only_desired_genes"
     only_genes["startIndex"], only_genes["endIndex"] = only_genes[["startIndex", "endIndex"]].min(axis=1), \
                                                        only_genes[["startIndex", "endIndex"]].max(axis=1)
 
+    # TODO change "only_genes" to "only_desired_genes"
     return only_genes
 
 
 def open_TE(te_file_name):
     NUMBER_OF_LINES_THAT_REPRESENTS_GFF_HEADER = 13
-    return pandas.read_csv(te_file_name, "\t", skiprows=NUMBER_OF_LINES_THAT_REPRESENTS_GFF_HEADER, header=None,
-                           names=["Chr", "Ver", "Type", "startIndex", "endIndex", "Irrelevant1", "Irrelevant2",
-                                  "Irrelevant3", "TEName"], compression=None).drop(
-        columns=["Irrelevant1", "Irrelevant2", "Irrelevant3", "Ver", "Type"])
+    df = pandas.read_csv(te_file_name, "\t", skiprows=NUMBER_OF_LINES_THAT_REPRESENTS_GFF_HEADER, header=None,
+                         names=["Chr", "Ver", "Type", "startIndex", "endIndex", "Irrelevant1", "Irrelevant2",
+                                "Irrelevant3", "TEName"], compression=None)
+    df.drop(columns=["Irrelevant1", "Irrelevant2", "Irrelevant3", "Ver", "Type", "TEName"])
+    df = df[df["Chr"] != "chrUn"]
+
+    df["startIndex"], df["endIndex"] = df[["startIndex", "endIndex"]].min(axis=1), \
+                                       df[["startIndex", "endIndex"]].max(axis=1)
+    return df
 
 
 def has_overlap(gene_start, gene_end, tra_start, tra_end):
@@ -78,27 +71,69 @@ def has_overlap(gene_start, gene_end, tra_start, tra_end):
     return True
 
 
-a = open_TE("PGSB_Transposon_annotation-v1__Triticum_aestivum_ArinaLrFor_v3.0.gff.gz")
+def get_te_gene_overlap(te_df, gene_df):
+    results = pandas.DataFrame({"chineseSpringGeneID": [], "overlap": []})
+    # for _, gene in tqdm.tqdm(gene_df.iterrows()):
+    for _, gene in tqdm.tqdm(gene_df.iloc[0:5,:].iterrows()):
+        chromosome_matched_transposons = te_df[te_df["Chr"] == gene["Chr"]]
+        start, end = gene["startIndex"], gene["endIndex"]
+        found_overlapping_transposon = False
+        for _, transposon in chromosome_matched_transposons.iterrows():
+            tra_start, tra_end = transposon["startIndex"], transposon["endIndex"]
+            if has_overlap(start, end, tra_start, tra_end):
+                found_overlapping_transposon = True
+                break
+        results = results.append({"chineseSpringGeneID": gene["chineseSpringGeneID"],
+                                  "overlap": found_overlapping_transposon}, ignore_index=True)
+    return results
 
-b = open_gene(r"D:\Code\WheatGenome\projectedGenes__Triticum_aestivum_ArinaLrFor_v3.0.gff")
 
-results = pandas.DataFrame({"chineseSpringGeneID": [], "overlap": []})
+def fetch_accessions_names():
+    TEs_directory = r"https://webblast.ipk-gatersleben.de/downloads/wheat/TE_annotation/Wheat_11__Transposons__via_Triticeae-TE-library/"
+    accession_name_and_version_regex = r'Triticum_aestivum_(.*)\.gff\.gz"'
+    return get_regex_from_web_pages(TEs_directory, accession_name_and_version_regex)
 
-for _, gene in tqdm.tqdm(b.iterrows()):
-    chromosome_matched_transposons = a[a["Chr"] == gene["Chr"]]
-    start, end = gene["startIndex"], gene["endIndex"]
-    found_overlapping_transposon = False
-    for _, transposon in chromosome_matched_transposons.iterrows():
-        tra_start, tra_end = transposon["startIndex"], transposon["endIndex"]
-        if has_overlap(start, end, tra_start, tra_end):
-            found_overlapping_transposon = True
-            break
-    results = results.append({"chineseSpringGeneID": gene["chineseSpringGeneID"],
-                              "overlap": found_overlapping_transposon}, ignore_index=True)
-    results.to_csv("results.csv")
 
-# download_genes()
-# download_TEs()
+def download_tdqm(path, out):
+    # Streaming, so we can iterate over the response.
+    response = requests.get(path, stream=True)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm.tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(out, 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    return out
 
-# TODO: Iterate on all accessions.
-# Add the acession name to the results dataframe
+
+def download_TE(accession_name):
+    TEs_directory = r"https://webblast.ipk-gatersleben.de/downloads/wheat/TE_annotation/Wheat_11__Transposons__via_Triticeae-TE-library/"
+    file_url = "{}PGSB_Transposon_annotation-v1__Triticum_aestivum_{}.gff.gz".format(TEs_directory, accession_name)
+    return download_tdqm(file_url, "{}_TE.gff".format(accession_name))
+
+
+def download_gene(accession_name):
+    TEs_directory = r"https://webblast.ipk-gatersleben.de/downloads/wheat/gene_projection/"
+    out_file_name = "{}_gene.gff"
+    try:
+        file_url = "{}projectedGenes__Triticum_aestivum_{}.gff".format(TEs_directory, accession_name)
+
+        return download_tdqm(file_url, out_file_name.format(accession_name))
+    except HTTPError:
+        file_url = "{}projectedGenes__Triticum_aestivum_{}_scaffolds.fasta.gz.gff".format(TEs_directory, accession_name)
+        return download_tdqm(file_url, out_file_name.format(accession_name))
+
+
+accessions = fetch_accessions_names()
+all_accessions_results = pandas.DataFrame({"chineseSpringGeneID": [], "overlap": [], "accession": []})
+# TODO: Remove the shuffle below
+random.shuffle(accessions)
+for accession in accessions:
+    results = get_te_gene_overlap(open_TE(download_TE(accession)), open_gene(download_gene(accession)))
+    results["accession"] = accession
+    results.to_csv("{}.results.csv".format(accession))
+    all_accessions_results = all_accessions_results.append(results)
+
+all_accessions_results.to_csv("results.csv")
